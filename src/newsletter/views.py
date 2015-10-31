86f90ef2766101c0d73345770a9565d1e7af52fd
Jared <jared.cain77@gmail.com>
2015-10-31 00:47:04 -0700

    Add email functionality first time

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.mail import send_mail
 from django.shortcuts import render
 
 from .forms import ContactForm, SignUpForm
@@ -52,17 +54,35 @@ def home(request):
 	return render(request, "home.html", context)
 
 
-''''''
+
 def contact(request):
 	form = ContactForm(request.POST or None)
 	if form.is_valid():
-		email = form.cleaned_data.get('email')
-		message = form.cleaned_data.get('message')
-		full_name = form.cleaned_data.get("full_name")
+		form_email = form.cleaned_data.get('email')
+		form_message = form.cleaned_data.get('message')
+		form_full_name = form.cleaned_data.get("full_name")
 
-		'''Print key and values to console with <dictionary>.items()'''
-		for key, value in form.cleaned_data.items():
-			print(key, value)
+		# '''Print key and values to console with <dictionary>.items()'''
+		# for key, value in form.cleaned_data.items():
+		# 	print(key, value)
+
+
+		'''TODO: Add comments on django email functionality'''
+		subject = 'Site contact form'
+		from_email = settings.EMAIL_HOST_USER
+		to_email = [from_email]
+
+		contact_message = "%s: %s via %s"%(
+				form_full_name,
+				form_message,
+				form_email)
+
+
+		send_mail(subject,
+			contact_message,
+			from_email,
+			to_email,
+			fail_silently=False)
 
 	context = {
 		"form": form
