4c8f797768ac1db82b52ffacd62f09af22005a05
Jared <jared.cain77@gmail.com>
2015-11-24 11:22:19 -0800

    Add Admin register for SomeUser

--- src/newsletter/admin.py
+++ src/newsletter/admin.py
@@ -1,11 +1,16 @@
+from django import forms
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
 # Register your models here.
 
 # Relative import .models
 from .models import SignUp
 # Relative import .forms
 from .forms import SignUpForm
+
+from accounts.models import SomeUser
+from accounts.forms.register import RegistrationForm
+
 
 '''Inherit the admin.ModelAdmin django object class'''
 class SignUpAdmin(admin.ModelAdmin):
@@ -17,4 +22,21 @@ class SignUpAdmin(admin.ModelAdmin):
 	form = SignUpForm
 
 '''Register admin site with SignUp model and custom admin page'''
+
+class MyUserAdmin(UserAdmin):
+	form = RegistrationForm
+
+	list_display = ('email', 'is_admin')
+	list_filter = ('is_admin',)
+	fieldsets = (
+		(None, {'fields': ('email',
+		'password')}),
+		('Permissions', {'fields': ('is_admin', 'is_superuser')})
+	)
+
+	search_fields = ('email',)
+	ordering = ('email',)
+	filter_horizontal = ()
+
 admin.site.register(SignUp, SignUpAdmin)
+admin.site.register(SomeUser, MyUserAdmin)
